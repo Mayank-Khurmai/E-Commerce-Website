4c3a9cdb8e3427d833c0f933e4f443eed876ff43
Shailendra Kumar <48162161+shailendrakrsk@users.noreply.github.com>
2019-09-15 02:25:17 +0530

    Update offer.php

--- offer.php
+++ offer.php
@@ -15,6 +15,7 @@ session_start();
 
 <!DOCTYPE html>
 <html>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
 <title>My Shop</title>
 <link rel="stylesheet" href="css.css">
