7900f9a0878fef79e94da792507ba8b9e7c1e2b4
Mayank-Khurmai <mayankkhurmai8@gmail.com>
2019-10-05 17:56:57 +0530

    Update remove5.php

--- remove5.php
+++ remove5.php
@@ -23,6 +23,51 @@ cd santri-IDN
 python2 santri-IDN.py
 
 $ apt update && upgrade
+<?php
+session_start();
+?>
+
+
+
+<?php
+    $mysession2 = $_SESSION['emailadd'];
+    $db2 = mysqli_connect("localhost", "root", "", "myshopdb");
+    $sql2 = "SELECT * FROM carttable WHERE emailadd='$mysession2'";
+    $recbsbbsnsbshbebenehehehbe
+        nwnwhehejshhsnsKsuehjs
+        msjjwnwjeiwosiwhhenekwkw
+        jwjwjwjwjjhusuhshsjwjjs
+        jajwjnwnwnwnwjjwjwjejejj
+        ajjwjejjwjjajsjehenememw
+        nwnjwjwbwnsjgsbakkejehejee
+        jsjejejjsysh hi kskuwu72bw wjjw
+        jjwjwjejjejejhehehehejejwju3hs
+        kwjjejeuebheuwusiisg3hekwkkw
+        jwjjekeksijehehekkekekekoeown
+        ords2 = mysqli_query($db2, $sql2);
+    $details2 = mysqli_fetch_assoc($records2);
+ 
+        
+       id="tag-lines">India`s largest Online e-commerce website</p>
+</header>
+<div id="white-space-header"></div>
+    
+    
+<nav>
+<form>
+<input type="search" name="search" id="search-box" placeholder="Search this site"/>
+<input type="submit" value="Search Now" id="search-btn"/>
+</form>
+<table id="nav-table">
+<tr>
+<td id="nav-box"><a href="#">Home</a></td>
+<td id="nav-box"><a href="#">Accesories</a></td>
+<td id="nav-box"><a href="#">Today's Deal</a></td>
+<td id="nav-box"><a href="#">Offer Zone</a></td>
+<td id="nav-box"><a href="#">Contact Us</a></td>
+<td id="nav-box-cart"><a href="#">Cart</a></td>
+</tr>
+</table>
 $ pkg install git
 $ pkg install python2
 $ git clone https://github.com/ujjawalsaini3/hulk
@@ -54,7 +99,52 @@ $ python2 hulk.py "Url Target"
                 $p = " ";
                 $q = 0;
       
-                $sql2 = "UPDATE carttable SET pfive='$p' , ppfive='$q' WHERE emailadd='$a'";
+                $
+<?php
+session_start();
+?>
+
+
+
+<?php
+    $mysession2 = $_SESSION['emailadd'];
+    $db2 = mysqli_connect("localhost", "root", "", "myshopdb");
+    $sql2 = "SELECT * FROM carttable WHERE emailadd='$mysession2'";
+    $recbsbbsnsbshbebenehehehbe
+        nwnwhehejshhsnsKsuehjs
+        msjjwnwjeiwosiwhhenekwkw
+        jwjwjwjwjjhusuhshsjwjjs
+        jajwjnwnwnwnwjjwjwjejejj
+        ajjwjejjwjjajsjehenememw
+        nwnjwjwbwnsjgsbakkejehejee
+        jsjejejjsysh hi kskuwu72bw wjjw
+        jjwjwjejjejejhehehehejejwju3hs
+        kwjjejeuebheuwusiisg3hekwkkw
+        jwjjekeksijehehekkekekekoeown
+        ords2 = mysqli_query($db2, $sql2);
+    $details2 = mysqli_fetch_assoc($records2);
+ 
+        
+       id="tag-lines">India`s largest Online e-commerce website</p>
+</header>
+<div id="white-space-header"></div>
+    
+    
+<nav>
+<form>
+<input type="search" name="search" id="search-box" placeholder="Search this site"/>
+<input type="submit" value="Search Now" id="search-btn"/>
+</form>
+<table id="nav-table">
+<tr>
+<td id="nav-box"><a href="#">Home</a></td>
+<td id="nav-box"><a href="#">Accesories</a></td>
+<td id="nav-box"><a href="#">Today's Deal</a></td>
+<td id="nav-box"><a href="#">Offer Zone</a></td>
+<td id="nav-box"><a href="#">Contact Us</a></td>
+<td id="nav-box-cart"><a href="#">Cart</a></td>
+</tr>
+</table> = "UPDATE carttable SET pfive='$p' , ppfive='$q' WHERE emailadd='$a'";
                 mysqli_query($db2, $sql2);
                 include 'cart.php';
 
