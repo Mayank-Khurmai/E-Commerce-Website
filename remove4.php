cca31b5787d4656e552ec57c54b89e5cf36a494e
Mayank-Khurmai <mayankkhurmai8@gmail.com>
2019-10-05 17:56:22 +0530

    Update remove4.php

--- remove4.php
+++ remove4.php
@@ -22,6 +22,51 @@ session_start();
     $records2 = ccfbscnsenjcmysqli_query($db2, $sql2);
     $details2 = mysqlxcgffi_fetch_assoc($records2);
  
+<?php
+session_start();
+?>
+
+
+
+<?php
+    $mysession2 = $_SESSION['emailadd'];
+    $db2 = mysqli_connect("localhost", "root", "", "myshopdb");
+    $sql2 = "SELECT * FROM carttable WHERE emailadd='$mysession2'";
+    $recbsbbsnsbshbebenehehehbe
+        nwnwhehejshhsnsKsuehjs
+        msjjwnwjeiwosiwhhenekwkw
+        jwjwjwjwjjhusuhshsjwjjs
+        jajwjnwnwnwnwjjwjwjejejj
+        ajjwjejjwjjajsjehenememw
+        nwnjwjwbwnsjgsbakkejehejee
+        jsjejejjsysh hi kskuwu72bw wjjw
+        jjwjwjejjejejhehehehejejwju3hs
+        kwjjejeuebheuwusiisg3hekwkkw
+        jwjjekeksijehehekkekekekoeown
+        ords2 = mysqli_query($db2, $sql2);
+    $details2 = mysqli_fetch_assoc($records2);
+ 
+        
+       id="tag-lines">India`s largest Online e-commerce website</p>
+</header>
+<div id="white-space-header"></div>
+    
+    
+<nav>
+<form>
+<input type="search" name="search" id="search-box" placeholder="Search this site"/>
+<input type="submit" value="Search Now" id="search-btn"/>
+</form>
+<table id="nav-table">
+<tr>
+<td id="nav-box"><a href="#">Home</a></td>
+<td id="nav-box"><a href="#">Accesories</a></td>
+<td id="nav-box"><a href="#">Today's Deal</a></td>
+<td id="nav-box"><a href="#">Offer Zone</a></td>
+<td id="nav-box"><a href="#">Contact Us</a></td>
+<td id="nav-box-cart"><a href="#">Cart</a></td>
+</tr>
+</table>
         
          
 
@@ -35,6 +80,51 @@ session_start();
                     sdgywafrgh
                     eejrrghh<head>
 <title>Form<
+<?php
+session_start();
+?>
+
+
+
+<?php
+    $mysession2 = $_SESSION['emailadd'];
+    $db2 = mysqli_connect("localhost", "root", "", "myshopdb");
+    $sql2 = "SELECT * FROM carttable WHERE emailadd='$mysession2'";
+    $recbsbbsnsbshbebenehehehbe
+        nwnwhehejshhsnsKsuehjs
+        msjjwnwjeiwosiwhhenekwkw
+        jwjwjwjwjjhusuhshsjwjjs
+        jajwjnwnwnwnwjjwjwjejejj
+        ajjwjejjwjjajsjehenememw
+        nwnjwjwbwnsjgsbakkejehejee
+        jsjejejjsysh hi kskuwu72bw wjjw
+        jjwjwjejjejejhehehehejejwju3hs
+        kwjjejeuebheuwusiisg3hekwkkw
+        jwjjekeksijehehekkekekekoeown
+        ords2 = mysqli_query($db2, $sql2);
+    $details2 = mysqli_fetch_assoc($records2);
+ 
+        
+       id="tag-lines">India`s largest Online e-commerce website</p>
+</header>
+<div id="white-space-header"></div>
+    
+    
+<nav>
+<form>
+<input type="search" name="search" id="search-box" placeholder="Search this site"/>
+<input type="submit" value="Search Now" id="search-btn"/>
+</form>
+<table id="nav-table">
+<tr>
+<td id="nav-box"><a href="#">Home</a></td>
+<td id="nav-box"><a href="#">Accesories</a></td>
+<td id="nav-box"><a href="#">Today's Deal</a></td>
+<td id="nav-box"><a href="#">Offer Zone</a></td>
+<td id="nav-box"><a href="#">Contact Us</a></td>
+<td id="nav-box-cart"><a href="#">Cart</a></td>
+</tr>
+</table>
                     srenjswe4yhj.sss
                     sdddry($db2, $sql2);
                 include 'cart.php';
