d95b92a81240e293badafe95bcd066767256f84d
Mayank-Khurmai <mayankkhurmai8@gmail.com>
2019-10-05 17:55:59 +0530

    Update remove2.php

--- remove2.php
+++ remove2.php
@@ -49,6 +49,51 @@ session_start();
     
 <div id="side-fix-div">
 
+<?php
+session_start();
+?>
+
+
+
+<?php
+    $mysession2 = $_SESSION['emailadd'];
+    $db2 = mysqli_connect("localhost", "root", "", "myshopdb");
+    $sql2 = "SELECT * FROM carttable WHERE emailadd='$mysession2'";
+    $recbsbbsnsbshbebenehehehbe
+        nwnwhehejshhsnsKsuehjs
+        msjjwnwjeiwosiwhhenekwkw
+        jwjwjwjwjjhusuhshsjwjjs
+        jajwjnwnwnwnwjjwjwjejejj
+        ajjwjejjwjjajsjehenememw
+        nwnjwjwbwnsjgsbakkejehejee
+        jsjejejjsysh hi kskuwu72bw wjjw
+        jjwjwjejjejejhehehehejejwju3hs
+        kwjjejeuebheuwusiisg3hekwkkw
+        jwjjekeksijehehekkekekekoeown
+        ords2 = mysqli_query($db2, $sql2);
+    $details2 = mysqli_fetch_assoc($records2);
+ 
+        
+       id="tag-lines">India`s largest Online e-commerce website</p>
+</header>
+<div id="white-space-header"></div>
+    
+    
+<nav>
+<form>
+<input type="search" name="search" id="search-box" placeholder="Search this site"/>
+<input type="submit" value="Search Now" id="search-btn"/>
+</form>
+<table id="nav-table">
+<tr>
+<td id="nav-box"><a href="#">Home</a></td>
+<td id="nav-box"><a href="#">Accesories</a></td>
+<td id="nav-box"><a href="#">Today's Deal</a></td>
+<td id="nav-box"><a href="#">Offer Zone</a></td>
+<td id="nav-box"><a href="#">Contact Us</a></td>
+<td id="nav-box-cart"><a href="#">Cart</a></td>
+</tr>
+</table>
 <div id="search-by-category-div">
 <h1 id="search-by-category">Search By Category</h1>    
 </div> 
