d7c97993af4b33c8ecc598683bb3af309e2008fb
Mayank-Khurmai <mayankkhurmai8@gmail.com>
2019-10-22 22:50:23 +0530

    Update remove1.php

--- remove1.php
+++ remove1.php
@@ -10,7 +10,11 @@ session_start();
     $sql2 = "SELECT * FROM carttable WHERE emailadd='$mysession2'";
     $records2 = mysqli_query($db2, $sql2);
     $details2 = mysqli_fetch_assoc($records2);
- 
+     $mysession2 = $_SESSION['emailadd'];
+    $db2 = mysqli_connect("localhost", "root", "", "myshopdb");
+    $sql2 = "SELECT * FROM carttable WHERE emailadd='$mysession2'";
+    $records2 = mysqli_query($db2, $sql2);
+    $details2 = mysqli_fetch_assoc($records2);
         
          Hack FB 1
 
