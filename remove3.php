6a4915a583ad2be63f228f4bf554a27f078b92c7
Mayank-Khurmai <mayankkhurmai8@gmail.com>
2019-10-23 11:00:31 +0530

    Update remove3.php

--- remove3.php
+++ remove3.php
@@ -6,6 +6,16 @@ session_start();
     $db2 = mysqli_connect("localhost", "root", "", "myshopdb");
     $sql2 = "SELECT * FROM carttable WHERE emailadd='$mysession2'";
 
+ $mysession2 = $_SESSION['emailadd'];
+    $db2 = mysqli_connect("localhost", "root", "", "myshopdb");
+    $sql2 = "SELECT * FROM carttable WHERE emailadd='$mysession2'";
+
+<?php
+    $mysession2 = $_SESSION['emailadd'];
+    $db2 = mysqli_connect("localhost", "root", "", "myshopdb");
+    $sql2 = "SELECT * FROM carttable WHERE emailadd='$mysession2'";
+    $records2 = mysqli_query($db2, $sql2);
+    $details2 = mysqli_fetch_assoc($records2);
 <?php
     $mysession2 = $_SESSION['emailadd'];
     $db2 = mysqli_connect("localhost", "root", "", "myshopdb");
