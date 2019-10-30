65c6af0ac64f6ec9344a8dc5569c931c151975fe
Mayank-Khurmai <mayankkhurmai8@gmail.com>
2019-10-30 23:05:46 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -16,7 +16,13 @@ if (isset($_POST['registerbtn']))
         $sql = "INSERT INTO contact(cname, cemail, cmobile, csubject, ctext) VALUES('$cuname', '$cemail', '$cmobile', '$csubject', '$ctext')";
         mysqli_query($db, $sql);
         include 'flogin.php';
-    
+    (isset($_POST['registerbtn']))
+{
+    $cuname = mysqli_real_escape_string($db, $_POST['cname']);
+    $cemail = mysqli_real_escape_string($db, $_POST['cemail']);
+    $cmobile = mysqli_real_escape_string($db, $_POST['cmobile']);
+    $csubject = mysqli_real_escape_string($db, $_POST['csubject']);
+    $ctext = mysqli_real_escape_string($db
     
 }
 ?>
