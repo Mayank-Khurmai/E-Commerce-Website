cb8f500d116d3b82a6984451882a2d96e42bd0f8
Mayank-Khurmai <mayankkhurmai8@gmail.com>
2019-11-07 21:40:55 +0530

    Update deleteproduct.php

--- deleteproduct.php
+++ deleteproduct.php
@@ -15,7 +15,9 @@ session_start();
 
 <!DOCTYPE html>
 <html>
-<head>
+<headhtnl
+Head.b
+Body
 <title>My Shop</title>
 <link rel="stylesheet" href="css.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
