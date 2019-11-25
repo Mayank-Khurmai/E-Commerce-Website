90c395d8a4cc05cb2da3ac4802c2e481c655596c
Mayank Khurmai <mayankkhurmai8@gmail.com>
2019-11-25 19:08:53 +0530

    Update deleteproduct.php

--- deleteproduct.php
+++ deleteproduct.php
@@ -23,6 +23,7 @@ Body
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="animate.css">
 <style>
+    body
 *{
     box-sizing: border-box;    
     }    
