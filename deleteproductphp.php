0e2c33f6e359b3c0781e923b2dd3691053c77a02
Mayank Khurmai <mayankkhurmai8@gmail.com>
2019-12-05 23:51:40 +0530

    Update deleteproductphp.php

--- deleteproductphp.php
+++ deleteproductphp.php
@@ -12,7 +12,32 @@ session_start();
     $sql2 = "DELETE FROM productinfo WHERE pid='$a' ";
     $records2 = mysqli_query($db2, $sql2);                
     include 'deleteproduct.php';
+*Hello Everyone!!*
 
+*MUJ ACM S-CHAP* is back with the newest edition of *BIGGEST HACKATHON* MUJ has ever witnessed!!👨🏻‍💻👩🏻‍💻
+
+                 🌐 *MUJ HACKS 4.0* 🌐
+ 
+_*A National level development competition*_ for testing the all round ability of the participants involved.
+
+Come up with the most innovative ideas and change your imagination into reality   with the applications which can solve our _*Real World Problems!*_
+
+_Register now_, for a chance to compete with the best and to win exciting _*cash prizes worth Rs.40,000*_ as well as *_$1000 BUGSEE credits, 100+ free T-shirts, stickers, food & many more goodies!_*.
+
+*✨(Registration Fee == NULL)✨*
+
+_Visit our website now, to know all about the Hack!_
+*https://mujhacks.co*
+
+*Registrations have begun!*
+
+_For further information, contact_
+
+*Rupanshu*
++91 97292 77811
+
+*Ishan*
++91 9628734492
 ?> Html
 Hello
 
