63cf60017c05f2271cbf112d0b5138c7bdd99547
Mayank Khurmai <mayankkhurmai8@gmail.com>
2019-12-26 20:17:08 +0530

    Update cart.php

--- cart.php
+++ cart.php
@@ -28,7 +28,7 @@ else
 <htmlll>
 <head>
 <title>My Shop</title>
-<link rel="stylesheet" href="css.css">
+<link rel="stylesheet" href="csts.css">
 <link rel="stylesheet" href="css2.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
